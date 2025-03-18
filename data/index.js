d29e166ed5ae523a469c33e02ea2cee507c391f3
Jacob Kelly <jskelly2021@gmail.com>
2025-03-17 23:45:33 -0500

    Local Express Server is succesfully running. Item data is fetched from database.

--- data/index.js
+++ data/index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 
 const { Pool } = require('pg');
+const express = require('express');
+
+const app = express();
+const port = 3000;
 
 const pool = new Pool({
     host: process.env.DB_HOST,
@@ -10,13 +14,23 @@ const pool = new Pool({
     port: process.env.DB_PORT || 5432,
 });
 
-pool.query('SELECT * FROM item;')
-    .then(result => {
-        console.log(result.rows);
-    })
-    .catch(err => {
-        console.error('Error querying the database:', err);
-    })
-    .finally(() => {
-        pool.end();
-    });
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API is working');
+});
+
+app.get('/api/items', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM item');
+        res.json(result.rows);
+    }
+    catch (err) {
+        console.error('Error executing query', err);
+        res.status(500).send('Server error');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});

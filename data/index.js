0698caa2aab499104ab00dcffedf5c1fe006d9d7
Jacob Kelly <jskelly2021@gmail.com>
2025-03-18 11:23:14 -0500

    Frontend can fetch data from backend app

--- data/index.js
+++ data/index.js
@@ -1,12 +1,13 @@
-const pool = require('./config/database');
 const express = require('express');
-
+const cors = require('cors');
+const pool = require('./config/database');
 const itemsRouter = require('./routes/items');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/items', itemsRouter);
 
 app.listen(port, () => {

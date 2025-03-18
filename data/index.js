133d6a7e8a72ae5ce49be4c3b4347e348da200aa
Jacob Kelly <jskelly2021@gmail.com>
2025-03-18 00:01:28 -0500

    Separated database config into separate file

--- data/index.js
+++ data/index.js
@@ -1,18 +1,8 @@
-require('dotenv').config();
-
-const { Pool } = require('pg');
+const pool = require('./config/database');
 const express = require('express');
 
 const app = express();
 const port = 3000;
-
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 5432,
-});
 
 app.use(express.json());
 

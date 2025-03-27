41cb90f7083e31284fa318bab1733ef8bbeaedc9
Jacob Kelly <jskelly2021@gmail.com>
2025-03-26 21:21:22 -0500

    React app gets data from backend

--- server/controllers/itemsController.js
+++ server/controllers/itemsController.js
@@ -4,11 +4,11 @@ import pool from '../config/database.js';
 export const getAllItems = async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM item');
-        res.status(200).json( { success: true, data: result.rows });
+        res.status(200).json(result.rows);
     }
     catch (err) {
         console.error('Error getAllItems', err);
-        res.status(500).json({ success: false, message: "Server Error" });
+        res.status(500).json("Server Error" );
     }
 }
 
@@ -17,10 +17,10 @@ export const getItem = async (req, res) => {
     const { id } = req.params;
     try {
         const result = await pool.query('SELECT * FROM item WHERE item_id=$1', [id]);
-        res.status(200).json({ success:true, data: result.rows });
+        res.status(200).json(result.rows);
     }
     catch (err) {
         console.error('Error getItem', err);
-        res.status(500).json({ success: false, message: "Item not found" });
+        res.status(500).json("Item not found");
     }
 }

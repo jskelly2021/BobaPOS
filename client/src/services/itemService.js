366119dd65e2a072fe64a7c4fdd46887032c3498
Jacob Kelly <jskelly2021@gmail.com>
2025-03-29 14:04:47 -0500

    Using axios for frontend api calls

--- client/src/services/itemService.js
+++ client/src/services/itemService.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 // Returns a list of all items from the backend
@@ -5,11 +7,7 @@ export const fetchItems = async () => {
     const itemURL = `${API_BASE_URL}/items`;
 
     try {
-        const response = await fetch(itemURL);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
+        const { data } = await axios.get(itemURL);
         return data;
     } catch (e) {
         throw new Error('Failed to fetch items: ${e.message)');

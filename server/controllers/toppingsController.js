53305260de4537444f4d49af20471493c94a83b3
dbehks3 <dahbinbehk@gmail.com>
2025-03-30 18:18:50 -0500

    added toppings endpoint and routes

---
+++ server/controllers/toppingsController.js
@@ -0,0 +1,41 @@
+import pool from '../config/database.js';
+
+//get all toppings
+export const getAllToppings = async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM topping');
+        res.status(200).json(result.rows);
+    }
+    catch (err) {
+        console.error('Error getAllToppings', err);
+        res.status(500).json("Server Error");
+    }
+}
+
+//get topping based on ID
+export const getTopping = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query('SELECT * FROM topping WHERE topping_id=$1', [id]);
+        res.status(200).json(result.rows);
+    }
+    catch (err) {
+        console.error('Error getTopping', err);
+        res.status(500).json("Server Error");
+    }
+}
+
+//update topping quantity
+export const updateToppingQuantity = async (req, res) => {
+    const { id } = req.params;
+    const { quantity } = req.body;
+    try {
+        const result = await pool.query('UPDATE topping SET quantity=$1 WHERE topping_id=$2 RETURNING *', [quantity, id]);
+        res.status(200).json(result.rows);
+        console.log(`Updating topping ${id}: New Quantity = ${quantity}`);
+    }
+    catch (err) {
+        console.error('Error updateToppingQuantity', err);
+        res.status(500).json("Server Error");
+    }
+}

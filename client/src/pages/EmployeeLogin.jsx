250543a46f343a5968da8ef9c99b729a833a17ee
Jacob Kelly <jskelly2021@gmail.com>
2025-04-09 11:20:00 -0500

    Toggling user mode stores the current mode in local storage

--- client/src/pages/EmployeeLogin.jsx
+++ client/src/pages/EmployeeLogin.jsx
@@ -4,17 +4,22 @@ import { useNavigate } from 'react-router-dom';
 function EmployeeLogin() {
     const nav = useNavigate();
 
-        return (
-            <div>
-                <button className='UserModeBtn' onClick={() => nav('/welcome')}>
-                    Toggle User Mode
-                </button>
-                <h1>Employee Login</h1>
-                <button className='LoginBtn' onClick={() => nav('/dashboard')}>
-                    Login
-                </button>
-            </div>
-        );
+    const handleToggleMode = () => {
+        localStorage.setItem("userMode", "customer");
+        nav('/welcome');
+    }
+
+    return (
+        <div>
+            <button className='UserModeBtn' onClick={() => handleToggleMode()}>
+                Toggle User Mode
+            </button>
+            <h1>Employee Login</h1>
+            <button className='LoginBtn' onClick={() => nav('/dashboard')}>
+                Login
+            </button>
+        </div>
+    );
 }
 
 export default EmployeeLogin;
--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -4,17 +4,22 @@ import { useNavigate } from 'react-router-dom';
 function EmployeeLogin() {
     const nav = useNavigate();
 
-        return (
-            <div>
-                <button className='UserModeBtn' onClick={() => nav('/')}>
-                    Toggle User Mode
-                </button>
-                <h1>Welcome</h1>
-                <button className='StartBtn' onClick={() => nav('/menu/customer')}>
-                    Start Order
-                </button>
-            </div>
-        );
+    const handleToggleMode = () => {
+        localStorage.setItem("userMode", "cashier");
+        nav('/');
+    }
+
+    return (
+        <div>
+            <button className='UserModeBtn' onClick={() => handleToggleMode()}>
+                Toggle User Mode
+            </button>
+            <h1>Welcome</h1>
+            <button className='StartBtn' onClick={() => nav('/menu/customer')}>
+                Start Order
+            </button>
+        </div>
+    );
 }
 
 export default EmployeeLogin;

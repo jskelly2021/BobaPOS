79cd62fea1f1a914b14e56d03ec40abd08785518
Jacob Kelly <jskelly2021@gmail.com>
2025-04-11 11:39:07 -0500

    Created a productUsage chart component

---
+++ client/src/components/analytics/ProductUsage.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import { LineChart } from 'recharts';
+import useAnalytics from '../../hooks/useAnalytics';
+
+const ProductUsage = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default ProductUsage;

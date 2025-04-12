4f8edbc7495e49f92e6be3e619ecb7bd776c77e4
Jacob Kelly <jskelly2021@gmail.com>
2025-04-12 12:22:18 -0500

    Created a hook to handle product usage data

--- client/src/components/analytics/ProductUsage.jsx
+++ client/src/components/analytics/ProductUsage.jsx
@@ -1,9 +1,14 @@
 
 import React from 'react'
 import { LineChart } from 'recharts';
-import useAnalytics from '../../hooks/useAnalytics';
+import useAnalytics from '../../hooks/useProductUsage';
 
 const ProductUsage = () => {
+    const { data, loading, error } = useSalesDuringDay(start, end);
+
+    if (loading) return <p>Loading data...</p>;
+    if (error) return <p>Error fetching ingredient usage data: {error.message}</p>;
+
     return (
         <div>
             Product Usage
---
+++ client/src/hooks/useProductUsage.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import {  } from '../services/analyticsService';
+
+const useProductUsage = () => {
+    const [data, setdata] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        
+    })
+
+}
+return { data, loading, error };
+
+export default useProductUsage;

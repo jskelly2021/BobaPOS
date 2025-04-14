5ed1e4d0c7cbc1deae7df901b5e6ab69eb8d0657
Jacob Kelly <jskelly2021@gmail.com>
2025-04-14 12:20:59 -0500

    Added axis titles and formatted the date ticks

--- client/src/components/analytics/ProductUsage.jsx
+++ client/src/components/analytics/ProductUsage.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import useProductUsage from '../../hooks/useProductUsage';
 import ProductUsageChart from './ProductUsageChart';
 import useIngredient from '../../hooks/useIngredient';
@@ -10,23 +10,30 @@ const ProductUsage = () => {
         getUsageFromLast3Months, getUsageFromLastYear, setIngredient } = useProductUsage();
     const { ingredients } = useIngredient();
 
+    const [ granularity, setGranularity ] = useState('daily');
+
+    const handleChangeData = (getUsage, newGranularity) => {
+        setGranularity(newGranularity);
+        getUsage(ingredientId);
+    }
+
     return (
         <div>
-            <ProductUsageChart loading={loading} error={error} data={usageData} />
+            <ProductUsageChart loading={loading} error={error} data={usageData} dateLabelType={granularity}/>
 
             <select value={ingredientId} onChange={(e) => setIngredient(e.target.value)}>
                 <option value="" disabled>Select an ingredient</option>
                 {ingredients.map(ingredient => (
                     <option key={ingredient.ingredient_id} value={ingredient.ingredient_id}>
-                        <button onClick={() => setIngredient(ingredient.ingredient_id)}>{ingredient.ingredient_name}</button>
+                        {ingredient.ingredient_name}
                     </option>
                 ))}
             </select>
 
-            <button onClick={() => getUsageFromLast24Hours(ingredientId)}>Last 24 Hours</button>
-            <button onClick={() => getUsageFromLast14Days(ingredientId)}>Last 14 Days</button>
-            <button onClick={() => getUsageFromLast3Months(ingredientId)}>Last 3 Months</button>
-            <button onClick={() => getUsageFromLastYear(ingredientId)}>Last Year</button>
+            <button onClick={() => handleChangeData(getUsageFromLast24Hours, 'hourly')}>Last 24 Hours</button>
+            <button onClick={() => handleChangeData(getUsageFromLast14Days, 'daily')}>Last 14 Days</button>
+            <button onClick={() => handleChangeData(getUsageFromLast3Months, 'daily')}>Last 3 Months</button>
+            <button onClick={() => handleChangeData(getUsageFromLastYear, 'monthly')}>Last Year</button>
 
         </div>
     );
--- client/src/components/analytics/ProductUsageChart.jsx
+++ client/src/components/analytics/ProductUsageChart.jsx
@@ -1,17 +1,47 @@
 
 import React from 'react'
-import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, Label } from 'recharts';
 
-const ProductUsageChart = ({ loading, error, data }) => {
+const ProductUsageChart = ({ loading, error, data, dateLabelType }) => {
     if (loading) return <p>Loading data...</p>;
     if (error) return <p>Error fetching ingredient usage data: {error.message}</p>;
 
+    const formatDate = (str) => {
+        const date = new Date(str);
+
+        switch (dateLabelType) {
+            case "hourly":
+                return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+            case "daily":
+                return date.toLocaleDateString(undefined, { month: 'short', day: 'numeric' });
+            case "monthly":
+                return date.toLocaleDateString(undefined, { month: 'short', year: 'numeric' });
+            default:
+                return date.toLocaleDateString();
+        }
+    };
+
     return (
         <div>
-            <LineChart width={600} height={300} data={data}>
+            <LineChart width={600} height={300} data={data} margin={{ top: 10, right: 20, left: 20, bottom: 15 }}>
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="period" />
-                <YAxis />
+                <XAxis dataKey="period" tickFormatter={formatDate}>
+                    <Label
+                            value='Date Time'
+                            dy={20} 
+                            position='inside bottom'
+                    />
+                </XAxis>
+
+                <YAxis>
+                    <Label
+                        value='Total Usage (kg)' 
+                        angle={-90}
+                        dx={-40}
+                        position='insideleft'
+                    />
+                </YAxis>
+
                 <Tooltip />
                 <Line type="monotone" dataKey="total_usage" stroke="#8884d8" />
             </LineChart>

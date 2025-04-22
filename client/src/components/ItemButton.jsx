bdc37b66141f02055fe401e00e6d7693dcf532f6
Jacob Kelly <jskelly2021@gmail.com>
2025-04-21 20:31:37 -0500

    Clicking an item in the customer view opens customization window

--- client/src/components/ItemButton.jsx
+++ client/src/components/ItemButton.jsx
@@ -2,17 +2,34 @@
 const ItemButton = ({ item, onClick }) => {
     return (
         <button className='ItemButton' onClick={() => onClick(item)}>
-            <img className='ItemImg' src={item.item_img} alt={item.item_name}></img>
+            <img className='ItemImg'
+                src={item.item_img}
+                alt={item.item_name}
+            />
+
             <div className='ItemDetails'>
-                <p className='ItemName'>{item.item_name}</p>
-                <p className='ItemPrice'>{item.price}</p>
-                <p className='ItemQuantity'>Quantity: {item.quantity}</p>
+                <p className='ItemName'>
+                    {item.item_name}
+                </p>
+
+                <p className='ItemPrice'>
+                    {item.price}
+                </p>
+
+                <p className='ItemQuantity'>
+                    Quantity: {item.quantity}
+                </p>
+
                 <p className='ItemTopping'>
-                    {item.toppings && item.toppings.length > 0 ? (
-                    item.toppings.map((topping) => (topping.quantity > 0 && (
-                    <p key={topping.topping_id}>
-                        {topping.topping_name} {topping.quantity === 1 ? "(Regular)" : topping.quantity === 0.5 ? "(Light)" : ""}
-                    </p>)))) : ( <p></p> )}
+                    {item.toppings && item.toppings.length > 0 
+                    ? (item.toppings.map((topping) =>
+                        (topping.quantity > 0 && (
+                            <p key={topping.topping_id}>
+                                {topping.topping_name} {topping.quantity === 1 ? "(Regular)" : topping.quantity === 0.5 ? "(Light)" : ""}
+                            </p>
+                        )))
+                    ) : ( <p></p> )
+                    }
                 </p>
             </div>
         </button>
